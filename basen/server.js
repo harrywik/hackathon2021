dbe4a4fef3c79fd4d43cdd56f03d7f6ddfc94532
Harry Wik <harrywiksmail@gmail.com>
2021-11-12 19:21:40 +0100

    webpages makes GET request

--- basen/server.js
+++ basen/server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.get("/", (req, res) => {
 
     const {exec} = require('child_process');
