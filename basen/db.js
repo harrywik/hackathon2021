b6d9edb651d11e90de265975c0068a01da420879
Harry Wik <harrywiksmail@gmail.com>
2021-11-13 09:58:44 +0100

    added .env for db-connection

--- basen/db.js
+++ basen/db.js
@@ -1,11 +1,12 @@
 const mariadb = require('mariadb');
+require('dotenv').config();
 
 const pool = mariadb.createPool({
-    host: "localhost",
-    port: 3306,
-    user: "harry",
-    password: "H@ckathon2021",
-    database: "Hackathon"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 module.exports = Object.freeze({
